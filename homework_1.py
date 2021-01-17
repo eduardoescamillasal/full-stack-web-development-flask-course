53148f825f2805252bfcd5ac777e1c1a7afd1db9
eduardoescamillasal <eduardo.escamillasal@gmail.com>
2021-01-17 17:28:30 -0600

    Basic Server Request with Hello World in HTML

---
+++ homework_1.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+
+def hello():
+    return """
+      <!DOCTYPE html>
+      <html>
+      <head><title>Hello</title></head>
+      <body><h1>Hello World</h1></body>
+      </html>
+      """, 200
+
+if __name__ =='__main__':
+    app.run(port=1729)
